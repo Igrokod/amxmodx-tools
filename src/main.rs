d25a41ec276b60168e85f72ecb014b7728c1e9e0
Fedcomp <aglergen@gmail.com>
2018-02-24 16:08:18 +0300

    File loading with shutdown macro

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,39 @@
 extern crate clap;
-use clap::App;
+
+use std::io::prelude::*;
+use std::fs::File;
+use clap::{Arg, App};
+
+// TODO: Custom panic handler
+macro_rules! die {
+    ($fmt:expr) => (print!(concat!($fmt, "\n")));
+    ($fmt:expr, $($arg:tt)*) => ({
+        print!(concat!($fmt, "\n"), $($arg)*);
+        std::process::exit(-1);
+    });
+}
 
 fn main() {
-    App::new("rxxma")
-       .version("0.0.1")
-       .about("Amxmodx plugin reverse utility")
-       .author("Fedcomp")
-       .get_matches();
+    let matches = App::new("rxxma")
+                   .version("0.0.1")
+                   .about("Amxmodx plugin reverse utility")
+                   .author("Fedcomp")
+                   .arg(Arg::with_name("file")
+                        .value_name("FILE")
+                        .help("amxmodx file to analyze")
+                        .required(true)
+                        .takes_value(true))
+                   .get_matches();
+
+    let file_path = matches.value_of("file").unwrap();
+    let mut file = match File::open(file_path) {
+        Ok(bin) => bin,
+        Err(e) => die!("Cannot open file: {}", e)
+    };
+
+    let mut contents: Vec<u8> = vec![];
+    match file.read_to_end(&mut contents) {
+        Ok(_) => (),
+        Err(e) => die!("Cannot read file: {}", e)
+    };
 }
