7fec4ffe4645e215c33168e50844e9f23aa69da4
Fedcomp <aglergen@gmail.com>
2018-02-24 19:44:52 +0300

    [rustfmt] Formatted

--- src/main.rs
+++ src/main.rs
@@ -17,30 +17,32 @@ macro_rules! die {
 
 fn main() {
     let matches = App::new("rxxma")
-                   .version("0.0.1")
-                   .about("Amxmodx plugin reverse utility")
-                   .author("Fedcomp")
-                   .arg(Arg::with_name("file")
-                        .value_name("FILE")
-                        .help("amxmodx file to analyze")
-                        .required(true)
-                        .takes_value(true))
-                   .get_matches();
+        .version("0.0.1")
+        .about("Amxmodx plugin reverse utility")
+        .author("Fedcomp")
+        .arg(
+            Arg::with_name("file")
+                .value_name("FILE")
+                .help("amxmodx file to analyze")
+                .required(true)
+                .takes_value(true),
+        )
+        .get_matches();
 
     let file_path = matches.value_of("file").unwrap();
     let mut file = match File::open(file_path) {
         Ok(bin) => bin,
-        Err(e) => die!("Cannot open file: {}", e)
+        Err(e) => die!("Cannot open file: {}", e),
     };
 
     let mut file_contents: Vec<u8> = Vec::new();
     match file.read_to_end(&mut file_contents) {
         Ok(_) => (),
-        Err(e) => die!("Cannot read file: {}", e)
+        Err(e) => die!("Cannot read file: {}", e),
     };
 
     let _amxmodx_file = match AmxmodxFile::from(&file_contents) {
         Ok(a) => a,
-        Err(e) => die!("File parsing error: {}", e)
+        Err(e) => die!("File parsing error: {}", e),
     };
 }
